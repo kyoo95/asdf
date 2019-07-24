3a974e2ed0134a61b2dde4adf78dbd70332f2d6d
Kevin <kevinyoo0203@yahoo.com>
2019-07-24 20:39:07 +0000

    initial commit

---
+++ assertObjectsEqual.js
@@ -0,0 +1,39 @@
+const eqObjects = function (object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } else {
+    for (let key in object1) {
+      if ((Array.isArray(object1[key]) && Array.isArray(object2[key]))) {
+        if (eqArrays(object1[key], object2[key]) === false) {
+          return false
+        }
+      } else {
+        if (object1[key] !== object2[key]) {
+          
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+}
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);   
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+}
+
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+console.log(assertObjectsEqual(ab, ba)); // => true
+  
+const abc = { a: "1", b: "2", c: "3" };
+console.log(assertObjectsEqual(ab, abc)); // => false
+  
+assertEqual(assertObjectsEqual(ab, ba), true);
+assertEqual(assertObjectsEqual(ab, abc), false);
