a6ce1d0cd9b07d75a36c65d62b777b2d724531ba
Kevin <kevinyoo0203@yahoo.com>
2019-07-25 19:24:28 +0000

    first commit

---
+++ findKey.js
@@ -0,0 +1,25 @@
+const findKey = function (object, callback) {
+  for (let value in object) {
+    if ((callback(object[value]))) {
+      return value;
+    }
+  }
+  return undefined;
+}
+
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+//   }
+// }
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)); // => "noma"
