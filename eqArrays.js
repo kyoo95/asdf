292ab6a5ebd425a611869cd62056228b4cca0c5e
Kevin <kevinyoo0203@yahoo.com>
2019-07-29 17:38:52 +0000

    refactor and added modules

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   for(let x = 0; x < arr1.length; x++) {
     if(arr1[x] !== arr2[x])
@@ -14,4 +6,5 @@ const eqArrays = function(arr1, arr2) {
   return true;
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+module.exports = eqArrays;
