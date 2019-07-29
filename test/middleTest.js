98074fe9de8aaee55babd1b789df4ddae86d7e84
Kevin <kevinyoo0203@yahoo.com>
2019-07-29 20:02:04 +0000

    refactored to use mocha and chai tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,22 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
-const eqArrays = require('../eqArrays');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const middle = require('../middle');
+// const eqArrays = require('../eqArrays');
 
-eqArrays(assertArraysEqual(middle([1]), []), true); // => []
-eqArrays(assertArraysEqual(middle([1, 2]), []), true); // => [])
-eqArrays(assertArraysEqual(middle([1, 2, 3]), [2]), true); // => [2]
-eqArrays(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]), true); // => [3])
-eqArrays(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]), true); // => [2, 3]
-eqArrays(assertArraysEqual(middle([1, 2, 3, 3, 5, 6]), [3,4]), true); // => [3, 4]);
+// eqArrays(assertArraysEqual(middle([1]), []), true); // => []
+// eqArrays(assertArraysEqual(middle([1, 2]), []), true); // => [])
+// eqArrays(assertArraysEqual(middle([1, 2, 3]), [2]), true); // => [2]
+// eqArrays(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]), true); // => [3])
+// eqArrays(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]), true); // => [2, 3]
+// eqArrays(assertArraysEqual(middle([1, 2, 3, 3, 5, 6]), [3,4]), true); // => [3, 4]);
+
+const assert = require('chai').assert;
+const middle = require('../middle');
+
+describe("middle", () => {
+  it("returns the middle number", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("return empty array if no middle number", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,14 @@
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const tail = require('../tail');
+
+
+// assertArraysEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-
-assertArraysEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+describe("#tail", () => {
+  it("returns 2, 3 for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+});
